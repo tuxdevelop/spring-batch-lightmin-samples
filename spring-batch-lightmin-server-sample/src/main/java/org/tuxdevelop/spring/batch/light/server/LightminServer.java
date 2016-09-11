ea955f75ccbbe87cf1a4c45091bda719afa9ac4c
tuxdevelop <tuxdevelop@gmail.com>
2016-09-11 19:08:17 +0200

    updated to lightmin 0.3.0 added lightmin sample server added Listener Job to addressMigrator

--- spring-batch-lightmin-server-sample/src/main/java/org/tuxdevelop/spring/batch/light/server/LightminServer.java
+++ spring-batch-lightmin-server-sample/src/main/java/org/tuxdevelop/spring/batch/light/server/LightminServer.java
@@ -2,10 +2,12 @@ package org.tuxdevelop.spring.batch.light.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.tuxdevelop.spring.batch.lightmin.client.configuration.EnableSpringBatchLightminClient;
 import org.tuxdevelop.spring.batch.lightmin.server.configuration.EnableSpringBatchLightminServer;
 
 @SpringBootApplication
 @EnableSpringBatchLightminServer
+@EnableSpringBatchLightminClient
 public class LightminServer {
 
 
