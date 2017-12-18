db2111fd5de33fddcbfd65cfcf44c0f38322d994
marbecker <marcel.becker@1und1.de>
2017-12-18 09:30:43 +0100

    added consul server sample

--- spring-batch-lightmin-server-sample-discovery-consul/src/main/java/org/tuxdevelop/spring/batch/lightmin/samples/server/ConsulDiscoveryClientLightminServer.java
+++ spring-batch-lightmin-server-sample-discovery-consul/src/main/java/org/tuxdevelop/spring/batch/lightmin/samples/server/ConsulDiscoveryClientLightminServer.java
@@ -1,4 +1,16 @@
 package org.tuxdevelop.spring.batch.lightmin.samples.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.tuxdevelop.spring.batch.lightmin.server.configuration.EnableSpringBatchLightminServer;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableSpringBatchLightminServer
 public class ConsulDiscoveryClientLightminServer {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ConsulDiscoveryClientLightminServer.class, args);
+    }
 }
